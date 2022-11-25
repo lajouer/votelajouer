e9a4a79f78f0aa71e119725e7f23e9f60cd9fb24
root <root@localhost.localdomain>
2022-11-25 19:11:21 +0900

    for render2

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const path = require('path');
 const express = require('express');
-const forceSsl = require('force-ssl-heroku');
+// const forceSsl = require('force-ssl-heroku');
 // const exec = require('child_process').exec;
 // サーバをインスタンス化する 
 const app = express();
+
+
 // http ⇒ https リダイレクト
-app.use(forceSsl);
+// app.use(forceSsl);
 
-// 以下の設定だけで dist/index.html も返せてはいる
-app.use(express.static(`${__dirname}/dist`));
+// // 以下の設定だけで dist/index.html も返せてはいる
+// app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}`));
 
-// ルートへのアクセス時は念のため dist/index.html を確実に返すようにしておく
+// // ルートへのアクセス時は念のため dist/index.html を確実に返すようにしておく
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/index.html`));
+//   res.sendFile(path.join(`${__dirname}/dist/index.html`));
+  res.sendFile(path.join(`${__dirname}/index.html`));
 });
-// サーバ起動
-const server = app.listen(process.env.PORT || 8080, () => {
+// // サーバ起動
+// const server = app.listen(process.env.PORT || 8080, () => {
+const server = app.listen(process.env.PORT || 3001, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(`Listening at http://${host}:${port}`);
